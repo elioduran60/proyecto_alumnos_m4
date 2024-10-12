dc613f7aa7cd8317be8c80862505c9dbce208a16
ELIO DURAN <elioduran60@gmail.com>
2024-10-12 12:14:48 -0300

    Esta es una prueba por ED 12-10-2024

--- alumnos_asiganturas/src/main/java/com/alumonsm4/Main.java
+++ alumnos_asiganturas/src/main/java/com/alumonsm4/Main.java
@@ -2,6 +2,8 @@ package com.alumonsm4;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Elianny!");
+
+
     }
 }
