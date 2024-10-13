fe7303d99e7867192d67bcdd03107f53a52297b2
ELIO DURAN <elioduran60@gmail.com>
2024-10-13 16:25:21 -0300

    Inicio de construccion de la clase Alumnos

--- alumnos_asiganturas/src/main/java/com/alumonsm4/modelos/Alumno.java
+++ alumnos_asiganturas/src/main/java/com/alumonsm4/modelos/Alumno.java
@@ -1,5 +1,14 @@
 package com.alumonsm4.modelos;
 
 public class Alumno {
+    private String nombre;
+    private String apellidos;
+    private String dni;
+
+    public Alumno(String nombre, String apellidos, String dni) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+    }
 
 }
